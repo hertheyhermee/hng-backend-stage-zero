089f128cddaa5e0599800cc2d231279f7ba03cd7
Azeez Tiamiyu <hayzed@MacBook-Air-6.local>
2026-04-13 23:52:12 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Endpoint is working!');
+});
 
 app.get('/api/classify', async (req, res) => {
     try {
